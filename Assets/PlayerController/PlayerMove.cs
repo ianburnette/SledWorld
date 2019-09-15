75e22d2a854bc937bcad729364b236645dbfeae5
ianburnette <ianburnette@yandex.com>
2019-09-15 17:31:52 +0200

    PlayerMove (feature): add support for sled movement using acceleration instead of forceChange

--- Assets/PlayerController/PlayerMove.cs
+++ Assets/PlayerController/PlayerMove.cs
@@ -22,11 +22,11 @@ public class PlayerMove : InputListener {
 	[Header("Private Variables")]
 	GroundDetection _groundDetection;
 	CharacterMotor _characterMotor;
+	PlayerSled _playerSled;
 	float _previousMovementAngle;
 	bool _pivoting;
 	Vector2 _currentInputVector;
 	Vector3 _movementDirectionRelativeToCamera, _currentMovementDirection;
-	
 	
 	protected override void SubscribeToInputListeners() => PlayerInput.OnMove += SetCurrentMovementVector;
 	protected override void UnsubscribeFromInputListeners() => PlayerInput.OnMove -= SetCurrentMovementVector;
@@ -38,34 +38,48 @@ public class PlayerMove : InputListener {
 		playerCollider = GetComponent<Collider>();
 		_characterMotor = GetComponent<CharacterMotor>();
 		_groundDetection = GetComponent<GroundDetection>();
+		_playerSled = GetComponent<PlayerSled>();
 	}
 
 	bool Grounded() => _groundDetection.GroundRelationship_.grounded;
-	void FixedUpdate() {
-		UpdatePlayerMovement();
-	}
+
+	bool OnSled() => _playerSled.OnSled;
+	
+	void FixedUpdate() => UpdatePlayerMovement();
 
 	void UpdatePlayerMovement() => ApplyMovementToMotor();
 
 	void ApplyMovementToMotor()
 	{
 		if (!currentProfile) return;
-		_characterMotor.MoveTo(
-			transform.position + _currentMovementDirection, 
-			currentProfile.movementAndRotationSpeedConfig.movementSpeed, 
-			ignoreY: false);
-
-		if (Grounded())
-		{
-			if (currentProfile.groundStickinessConfig)
-				_characterMotor.ApplyAdditionalForce(GroundStickinessForce());
-			_characterMotor.ApplyAdditionalForce(SlopeUp());
-		}
+		
+		ApplyVelocityChange();
+		ApplyAcceleration();
 
 		_characterMotor.RotateToVelocity(currentProfile.movementAndRotationSpeedConfig.rotationSpeed, true);
 		_characterMotor.LimitSpeed(false);
-	}
 
+		void ApplyVelocityChange()
+		{
+			if (OnSled()) return;
+			_characterMotor.MoveTo(
+				MovementDirection(), currentProfile.movementAndRotationSpeedConfig.movementSpeed, ignoreY: false);
+		}
+		
+		void ApplyAcceleration()
+		{
+			if (!Grounded()) return;
+			if (currentProfile.groundStickinessConfig)
+				_characterMotor.ApplyAdditionalForce(GroundStickinessForce());
+			if (OnSled())
+				_characterMotor.ApplyAdditionalForce(
+					MovementDirection() * currentProfile.movementAndRotationSpeedConfig.movementSpeed
+					);
+			_characterMotor.ApplyAdditionalForce(OnSled() ? SlopeDown() : SlopeUp());
+		}
+		
+		Vector3 MovementDirection() => transform.position + _currentMovementDirection;
+	}
 	
 	Vector3 GroundStickinessForce() => 
 		-_groundDetection.GroundRelationship_.groundNormal * currentProfile.groundStickinessConfig.groundStickForce;
@@ -75,4 +89,7 @@ public class PlayerMove : InputListener {
 	bool IsOnSteepSlope() => _groundDetection.GroundRelationship_.groundAngle > MaxWalkableSlopeAngle;
 	Vector3 SlopeUp() => _groundDetection.SlopeCorrection() * -Physics.gravity.magnitude;
 
+	Vector3 SlopeDown() =>
+		_groundDetection.SlopeCorrection() *
+		(currentProfile.sledFrictionConfig ? currentProfile.sledFrictionConfig.slopeDownForce : 0f);
 }
